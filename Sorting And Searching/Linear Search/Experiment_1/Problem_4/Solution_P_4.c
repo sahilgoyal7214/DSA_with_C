3f50500e6b3beb05bd7651b39386bd7aeb2232e6
Unicorn <114812731+sahilgoyal7214@users.noreply.github.com>
2023-07-31 11:32:04 +0530

    Update Solution_P_4.c

--- Sorting And Searching/Linear Search/Experiment_1/Problem_4/Solution_P_4.c
+++ Sorting And Searching/Linear Search/Experiment_1/Problem_4/Solution_P_4.c
@@ -1,63 +1,36 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-
-int binarySearchWords(char* words[], int size, char* target) {
-    int start = 0;
-    int end = size - 1;
-
-    while (start <= end) {
-        int mid = start + (end - start) / 2;
-        int compare = strcmp(words[mid], target);
-
-        if (compare == 0) {
-            return mid; // Found the target word, return the index
-        } else if (compare > 0) {
-            end = mid - 1; // Target word is before the mid word
-        } else {
-            start = mid + 1; // Target word is after the mid word
-        }
-    }
-
-    return -1; // Target word not found
+int binarySearchForWord(char arr[][100], int left, int right, char target[]) {
+ while (left <= right) {
+ int mid = left + (right - left) / 2;
+ int cmp = strcmp(arr[mid], target);
+ if (cmp == 0) {
+ return mid;
+ } else if (cmp < 0) {
+ left = mid + 1;
+ } else {
+ right = mid - 1; 
+ }
+ }
+ return -1;
 }
-
 int main() {
-    int size, i;
-    printf("Enter the number of words: ");
-    scanf("%d", &size);
-    getchar(); // Clear the newline character left in the input buffer
-
-    char** words = (char**)malloc(size * sizeof(char*));
-
-    printf("Enter the sorted words:\n");
-    for (i = 0; i < size; i++) {
-        char buffer[100];
-        fgets(buffer, sizeof(buffer), stdin);
-        buffer[strcspn(buffer, "\n")] = '\0'; // Remove the trailing newline character from the input
-
-        words[i] = (char*)malloc((strlen(buffer) + 1) * sizeof(char));
-        strcpy(words[i], buffer);
-    }
-
-    char target[100];
-    printf("Enter the target word to search for: ");
-    fgets(target, sizeof(target), stdin);
-    target[strcspn(target, "\n")] = '\0'; // Remove the trailing newline character from the input
-
-    int index = binarySearchWords(words, size, target);
-
-    if (index != -1) {
-        printf("The target word '%s' is found at index %d.\n", target, index);
-    } else {
-        printf("The target word '%s' is not found in the array.\n", target);
-    }
-
-    // Free dynamically allocated memory
-    for (i = 0; i < size; i++) {
-        free(words[i]);
-    }
-    free(words);
-
-    return 0;
+ int n;
+ printf("Enter the number of words in the sorted array: ");
+ scanf("%d", &n);
+ char words[n][100];
+ printf("Enter %d words in lexicographic order:\n", n);
+ for (int i = 0; i < n; i++) {
+ scanf("%s", words[i]);
+ }
+ char target[100];
+ printf("Enter the target word to find: ");
+ scanf("%s", target);
+ int index = binarySearchForWord(words, 0, n - 1, target);
+ if (index != -1) {
+ printf("The target word '%s' is found at index: %d\n", target, index);
+ } else {
+ printf("The target word '%s' is not found in the array.\n", target);
+ }
+ return 0;
 }
